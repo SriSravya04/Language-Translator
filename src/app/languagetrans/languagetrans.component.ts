06876d90156176c1c373d7f265bd99e1ba27e9df
SriSravya04 <srisravya.nc04@gmail.com>
2024-04-15 19:14:06 +0530

    Language and url changed

--- src/app/languagetrans/languagetrans.component.ts
+++ src/app/languagetrans/languagetrans.component.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { languages } from '../Models/languages'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-languagetrans',
   standalone: true,
   imports: [
     FormsModule,
+    CommonModule,
     ReactiveFormsModule,
   ],
  
@@ -17,21 +19,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class LanguagetransComponent {
   fromText : any;
   toText : any
-
+  lang = languages
+  fromLang : any;
+  toLang : any
   constructor(private http:HttpClient){}
 
-  translate(){
+  translate(){    
     this.fromText
     this.toText
     let text = this.fromText.trim();
 
-    let apiUrl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ "en" + "&tl=" + "hi" +"&dt=t&q=" + encodeURI(text);
+    let apiUrl = "https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl="+ this.fromLang + "&tl="+ this.toLang +"&q=" + encodeURI(text);
 
     this.http.get(apiUrl).subscribe((res: any)=>
     {
-      console.log(res[0][0][0]);
+      console.log(res[0]);
       
-      this.toText=res[0][0][0];
+      this.toText=res[0];
 
     })
 
